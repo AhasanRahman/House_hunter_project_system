efe73fe2f9afe17807969a248bd778c6e3cb656a
AhasanRahman <shakilahasan98@gmail.com>
2023-10-15 18:03:26 +0600

    slideradd.

--- script.js
+++ script.js
@@ -26,3 +26,33 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "flex";
 //   dots[slideIndex-1].className += " active";
 }
+
+
+
+
+// // auto slide
+
+let slideIndexb = 0;
+showSlide();
+
+function showSlide() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexb++;
+  if (slideIndexb > slides.length) {slideIndexb = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexb-1].style.display = "flex";  
+  dots[slideIndexb-1].className += " active";
+  setTimeout(showSlide, 2000);  
+}
+
+
+
+
+ 
